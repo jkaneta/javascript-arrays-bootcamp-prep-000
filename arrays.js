582a4c444de62de264d457b34a30c95b2fc7d8b4
J <flatironschool@2joyce.com>
2018-01-12 20:15:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,13 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
   return array;
 }
