9c519d2cf6db79a1c3b7d98e5424a5b795322222
J <flatironschool@2joyce.com>
2018-01-12 19:39:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
+  return (array.unshift(element));
+  //return array;
 }
