f5235fc6b7651738aa4fc84c6888f84c949b08c8
J <flatironschool@2joyce.com>
2018-01-12 19:38:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,5 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = [element, ...array];
-  return array;
+  return array.unshift(element);
 }
